037adda882b5184d7698255f02bf6f8bd3b2ceb5
= <=>
2026-01-06 09:06:55 +0700

    Relocate welcome jumbotron from Home to Admin Dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -8,6 +8,23 @@
 
 @section('content_title', 'Dashboard')
 @section('content')
+  <div class="row mb-4">
+    <div class="col-lg">
+      <div class="custom-jumbotron">
+        @role('admin|petugas')
+          <h1 class="display-4 font-weight-bold">Halo, {{ Universe::petugas()->nama_petugas }}!</h1>
+        @endrole
+
+        @role('siswa')
+          <h1 class="display-4 font-weight-bold">Halo, {{ Universe::siswa()->nama_siswa }}!</h1>
+        @endrole
+        <p class="lead" style="font-weight: 500;">Selamat datang di Sistem Pembayaran SPP Modern.</p>
+        <hr class="my-4" style="background-color: rgba(255,255,255,0.2);">
+        <p>Kelola data pembayaran dengan lebih efisien dan modern.</p>
+      </div>
+    </div>
+  </div>
+
   <!-- Modern Stats Section -->
   <div class="row">
     <div class="col-lg-3 col-6">
