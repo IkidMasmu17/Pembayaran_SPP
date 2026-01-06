564f07ad589df228748018967115fc32d838ac2c
= <=>
2026-01-06 09:04:16 +0700

    Modernize dashboard and home views with new theme components

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -2,124 +2,112 @@
 @section('title', 'Dashboard')
 
 @push('css')
-<link rel="stylesheet" type="text/css" href="{{ asset('templates/backend/AdminLTE-3.1.0') }}/plugins/chart.js/Chart.min.css">
+  <link rel="stylesheet" type="text/css"
+    href="{{ asset('templates/backend/AdminLTE-3.1.0') }}/plugins/chart.js/Chart.min.css">
 @endpush
 
 @section('content_title', 'Dashboard')
 @section('content')
-<!-- Small boxes (Stat box) -->
-<div class="row">
-  <div class="col-lg-3 col-6">
-    <!-- small box -->
-    <div class="small-box bg-info">
-      <div class="inner">
-        <h3>{{ $total_siswa }}</h3>
-
-        <p>Siswa</p>
-      </div>
-      <div class="icon">
-        <i class="fas fa-users"></i>
-      </div>
-      <a href="{{ route('siswa.index') }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-    </div>
-  </div>
-  <!-- ./col -->
-  <!-- ./col -->
-  <div class="col-lg-3 col-6">
-    <!-- small box -->
-    <div class="small-box bg-danger">
-      <div class="inner">
-        <h3>{{ $total_kelas }}</h3>
-
-        <p>Kelas</p>
-      </div>
-      <div class="icon">
-        <i class="fas fa-school"></i>
-      </div>
-      <a href="{{ route('kelas.index') }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-    </div>
-  </div>
-  <!-- ./col -->
-  <div class="col-lg-3 col-6">
-    <!-- small box -->
-    <div class="small-box bg-success">
-      <div class="inner">
-        <h3>{{ $total_petugas }}</h3>
-
-        <p>Petugas</p>
-      </div>
-      <div class="icon">
-        <i class="fas fa-user-tie"></i>
-      </div>
-      <a href="{{ route('petugas.index') }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-    </div>
-  </div>
-  <!-- ./col -->
-  <div class="col-lg-3 col-6">
-    <!-- small box -->
-    <div class="small-box bg-warning">
-      <div class="inner">
-        <h3>{{ $total_admin }}</h3>
-
-        <p>Admin</p>
-      </div>
-      <div class="icon">
-        <i class="fas fa-user-secret"></i>
-      </div>
-      <a href="{{ route('admin-list.index') }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-    </div>
-  </div>
-</div>
-<!-- /.row -->
-
-<div class="container mt-5">
-    <div class="row">
-        <div class="col-md-10 offset-md-1">
-            <div class="card">
-                <div class="card-header">Siswa</div>
-                <div class="card-body">
-                    <canvas id="canvas" height="200" width="500"></canvas>
-                </div>
-            </div>
+  <!-- Modern Stats Section -->
+  <div class="row">
+    <div class="col-lg-3 col-6">
+      <div class="stats-card">
+        <div class="stats-icon icon-blue">
+          <i class="fas fa-users"></i>
         </div>
+        <div class="stats-info">
+          <h3>{{ $total_siswa }}</h3>
+          <p>Total Siswa</p>
+        </div>
+      </div>
     </div>
-</div>
+    <div class="col-lg-3 col-6">
+      <div class="stats-card">
+        <div class="stats-icon icon-red">
+          <i class="fas fa-school"></i>
+        </div>
+        <div class="stats-info">
+          <h3>{{ $total_kelas }}</h3>
+          <p>Total Kelas</p>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-3 col-6">
+      <div class="stats-card">
+        <div class="stats-icon icon-green">
+          <i class="fas fa-user-tie"></i>
+        </div>
+        <div class="stats-info">
+          <h3>{{ $total_petugas }}</h3>
+          <p>Total Petugas</p>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-3 col-6">
+      <div class="stats-card">
+        <div class="stats-icon icon-orange">
+          <i class="fas fa-user-secret"></i>
+        </div>
+        <div class="stats-info">
+          <h3>{{ $total_admin }}</h3>
+          <p>Total Admin</p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="row mt-4">
+    <div class="col-md-12">
+      <div class="card shadow-soft border-0" style="border-radius: 20px;">
+        <div class="card-header bg-transparent border-0 pt-4 px-4">
+          <h5 class="card-title font-weight-bold" style="color: #1e293b;">Statistik Siswa</h5>
+          <p class="text-sm text-muted">Perbandingan jumlah siswa berdasarkan jenis kelamin</p>
+        </div>
+        <div class="card-body px-4 pb-4">
+          <div class="chart-container-modern">
+            <canvas id="canvas" height="300" style="max-height: 400px;"></canvas>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 @endsection
 
 @push('js')
-<script type="text/javascript" src="{{ asset('templates/backend/AdminLTE-3.1.0') }}/plugins/chart.js/Chart.min.js"></script>
-<script>
-var ctx = document.getElementById("canvas").getContext('2d');
-  var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-      labels: ["Siswa Laki-laki", "Siswa Perempuan"],
-      datasets: [{
-        label: '',
-        data: [
-        {!! $siswa_laki_laki !!},
-        {!! $siswa_perempuan !!},
-        ],
-        backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        ],
-        borderColor: [
-        'rgba(255,99,132,1)',
-        'rgba(54, 162, 235, 1)',
-        ],
-        borderWidth: 1
-      }]
-    },
-    options: {
-      scales: {
-        yAxes: [{
-          ticks: {
-            beginAtZero:true
-          }
+  <script type="text/javascript"
+    src="{{ asset('templates/backend/AdminLTE-3.1.0') }}/plugins/chart.js/Chart.min.js"></script>
+  <script>
+    var ctx = document.getElementById("canvas").getContext('2d');
+    var myChart = new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: ["Siswa Laki-laki", "Siswa Perempuan"],
+        datasets: [{
+          label: '',
+          data: [
+            {!! $siswa_laki_laki !!},
+            {!! $siswa_perempuan !!},
+          ],
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+          ],
+          borderColor: [
+            'rgba(255,99,132,1)',
+            'rgba(54, 162, 235, 1)',
+          ],
+          borderWidth: 1
         }]
+      },
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
+            }
+          }]
+        }
       }
-    }
-  });
-</script>
+    });
+  </script>
 @endpush
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,20 +2,21 @@
 @section('title', 'Home')
 @section('content_title', 'Home')
 @section('content')
-<x-alert></x-alert>
-<div class="row">
-	<div class="col-lg">
-		<div class="jumbotron">
-		@role('admin|petugas')
-		  <h1 class="display-4">Hello, {{ Universe::petugas()->nama_petugas }}!</h1>
-		@endrole
+	<x-alert></x-alert>
+	<div class="row">
+		<div class="col-lg">
+			<div class="custom-jumbotron">
+				@role('admin|petugas')
+				<h1 class="display-4 font-weight-bold">Halo, {{ Universe::petugas()->nama_petugas }}!</h1>
+				@endrole
 
-		@role('siswa')
-		  <h1 class="display-4">Hello, {{ Universe::siswa()->nama_siswa }}!</h1>
-		@endrole
-		  <p class="lead">Selamat datang di WEB SPPR.</p>
-		  <hr class="my-4">
+				@role('siswa')
+				<h1 class="display-4 font-weight-bold">Halo, {{ Universe::siswa()->nama_siswa }}!</h1>
+				@endrole
+				<p class="lead" style="font-weight: 500;">Selamat datang di Sistem Pembayaran SPP Modern.</p>
+				<hr class="my-4" style="background-color: rgba(255,255,255,0.2);">
+				<p>Kelola data pembayaran dengan lebih efisien dan modern.</p>
+			</div>
 		</div>
 	</div>
-</div>
 @endsection
